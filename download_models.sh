7be9966b3183dc64316d921deba8967c6b44a5c1
bijoycp <bijoycp90@gmail.com>
2024-03-23 16:02:45 +0530

    yolov8 jetson nano all code added

--- download_models.sh
+++ download_models.sh
@@ -2,7 +2,6 @@ pip3 install gdown
 mkdir model
 
 #yolov8n
-gdown https://drive.usercontent.google.com/download?id=1Qmh0y9XhyeMl5hrz-bQUMIVJsck37Znc
+gdown gdown https://drive.google.com/uc\?id=1Qmh0y9XhyeMl5hrz-bQUMIVJsck37Znc
 
 mv *.onnx model/.
-
